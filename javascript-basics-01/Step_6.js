8e74ec9f06a83790481fe8cef0b6ae8ba04f894c
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-07 15:46:54 +0200

    modified step-6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,4 +1,7 @@
 const num1 = parseInt(prompt('Enter the first number '));
+document.getElementById("first_number").value=num1;
 const num2 = parseInt(prompt('Enter the second number '));
+document.getElementById("second_number").value=num2;
 const remdr = num1 % num2;
-alert(`The remainder of ${num1} and ${num2} is ${remdr}`);
+document.getElementById("validate").onclick = function() {
+alert(`The remainder of ${num1} and ${num2} is ${remdr}`);}
