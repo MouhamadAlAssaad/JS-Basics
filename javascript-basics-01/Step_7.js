41a4c92441edf001a2447436eb9bd88384767f21
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-07 15:49:42 +0200

    modified step-7

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,10 +1,13 @@
 const num1 = parseInt(prompt('Enter your shoe size '));
+document.getElementById("shoe_size").value=num1;
 const num2 = parseInt(prompt('Enter your birth year '));
+document.getElementById("year").value=num2;
 function myFunction(num, num2) {
     let x = (((((num*2)+5)*50)-num2)+1766);
 
     return x;
   }
-
+  document.getElementById("validate").onclick = function() {
   let y =myFunction(num1,num2);
   alert(y);
+  }
