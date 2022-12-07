d1c04fdaaaad1840026ade9c41f5da4ce9019898
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-07 13:13:33 +0200

    finish step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,11 @@
+const age = parseInt(prompt("Enter your age"));
+
+let answer;
+
+if (age>18) {
+    answer = "You are over 18";}
+else {
+    answer =  "You are under 18";
+};
+console.log(answer)
+alert(answer)
