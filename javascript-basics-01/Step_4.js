28880ee08eea27d24deca842392aeee42999acfe
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-07 10:45:00 +0200

    finish step_4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,4 +1,9 @@
 var name=prompt("Enter your name");
 var surname=prompt("Enter your surname");
 var city=prompt("Enter your city");
-alert(` hello  ${name} `)
+alert(` hello
+    name : ${name}
+    surname : ${surname}
+    city : ${city}
+
+    `);
