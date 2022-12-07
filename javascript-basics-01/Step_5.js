56c9eaea2d8dd92aa6f8fe45af6da874a2594637
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-07 11:24:26 +0200

    modified step_5

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,4 +1,4 @@
 const num1 = parseInt(prompt('Enter the first number '));
 const num2 = parseInt(prompt('Enter the second number '));
 const mult = num1 * num2;
-alert(`The sum of ${num1} and ${num2} is ${mult}`);
+alert(`The multiplication of ${num1} and ${num2} is ${mult}`);
---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,4 @@
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+const mult = num1 * num2;
+alert(`The mult of ${num1} and ${num2} is ${mult}`);
