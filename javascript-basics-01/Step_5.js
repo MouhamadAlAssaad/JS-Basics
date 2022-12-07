0d83b1741abc5b084102a6068d8b8f32af4529e0
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-07 15:44:25 +0200

    modified step-5

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,4 +1,7 @@
 const num1 = parseInt(prompt('Enter the first number '));
+document.getElementById("first_number").value=num1;
 const num2 = parseInt(prompt('Enter the second number '));
+document.getElementById("second_number").value=num2;
 const mult = num1 * num2;
-alert(`The multiplication of ${num1} and ${num2} is ${mult}`);
+document.getElementById("validate").onclick = function() {
+alert(`The multiplication of ${num1} and ${num2} is ${mult}`);}
