212201fbbe5fd70e2aefdbdf84f43b4b25e52e3d
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-07 15:39:35 +0200

    modified step-4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,9 +1,14 @@
 var name=prompt("Enter your name");
+document.getElementById("name").value=name;
 var surname=prompt("Enter your surname");
+document.getElementById("surname").value=surname;
 var city=prompt("Enter your city");
+document.getElementById("city").value=city;
+document.getElementById("validate").onclick = function() {
 alert(` hello
     name : ${name}
     surname : ${surname}
     city : ${city}
 
     `);
+}
