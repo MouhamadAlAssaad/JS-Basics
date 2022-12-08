c972fa1851b9cc0f84844cd652b89953560320b5
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-08 13:14:59 +0200

    finished step4

---
+++ javascript-basics-03/step 4/Step_4.js
@@ -0,0 +1,12 @@
+let password = document.getElementById("password");
+let confirmation = document.getElementById("confirmation");
+
+document.querySelector("button").onclick = function(e){
+    if(password.value == '' && confirmation.value == ''){
+        password.style.border = "1px solid red";
+    } else if(password.value == confirmation.value){
+        alert("Password Confirmed!");
+    } else {
+        confirmation.style.border = "1px solid red"
+    }
+};
